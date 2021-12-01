a9f1fe16b9132b81af7463f7590a7558f4069648
Kaio Torres <57267578+KaioTorrers19@users.noreply.github.com>
2021-12-01 13:30:22 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,4 @@
+number1= int(input('coloque o primeiro numero para somar'))
+number2= int(input('coloque o segundo numero para somar'))
+resultado = number1 + number2
+print('a soma dára o valor de{}'.format(resultado))
